52e117822e62629584480ac06b1e55e921b18a6a
mrfinker <caalebs@gmail.com>
2024-01-31 12:19:08 +0000

    31 janv. 2024, 13:19

--- index.js
+++ index.js
@@ -14,20 +14,22 @@
 // alert("La valeur de "+ far +` Faraneiht en Celsius est : ${(cel * 9 / 5) + 32}`)
 
 // 3.Strings
-console.log("Caleb" + " Kiangebeni")
-console.log("Caleb" + " Kiangebeni" + " Demissa")
-console.log(typeof(2))
-console.log(typeof(2.2))
-console.log(typeof(2,2))
-console.log(typeof("Bonjour"))
-console.log("hello"+3)
-console.log("$" + 500 + 300)
-console.log("$500" + 300)
-console.log("$" + (500 + 300))
-console.log("Bonjour")
-console.log('Bonjour')
-console.log('Je t\'emmerde')
-let Hey = "Bonjour"
-console.log(`${Hey}`)
-console.log(`Bonjour
-tu vas bien ?`)
+// console.log("Caleb" + " Kiangebeni")
+// console.log("Caleb" + " Kiangebeni" + " Demissa")
+// console.log(typeof(2))
+// console.log(typeof(2.2))
+// console.log(typeof(2,2))
+// console.log(typeof("Bonjour"))
+// console.log("hello"+3)
+// console.log("$" + 500 + 300)
+// console.log("$500" + 300)
+// console.log("$" + (500 + 300))
+// console.log("Bonjour")
+// console.log('Bonjour')
+// console.log('Je t\'emmerde')
+// let Hey = "Bonjour"
+// console.log(`${Hey}`)
+// console.log(`Bonjour
+// tu vas bien ?`)
+
+console.log('My name is : as a', typeof("Caleb Kiangebeni"));
