7feb889dcf24965b9ce83f0106c85d36aadf0e26
mrfinker <caalebs@gmail.com>
2024-01-31 23:14:49 +0000

    1 févr. 2024, 00:14

--- index.js
+++ index.js
@@ -59,11 +59,11 @@ Multi-ligne */
 // console.log(`Total cost : $${totalCost}`);
 
 // 6. Booelean and if statement
-console.log(3>5);
-console.log(3<5);
-console.log(3<5+2);
-console.log(3<5-3);
-console.log(typeof(true));
-console.log(typeof('true'));
-console.log(5 == '5');
-console.log(5 === '5');
+// console.log(3>5);
+// console.log(3<5);
+// console.log(3<5+2);
+// console.log(3<5-3);
+// console.log(typeof(true));
+// console.log(typeof('true'));
+// console.log(5 == '5');
+// console.log(5 === '5');
