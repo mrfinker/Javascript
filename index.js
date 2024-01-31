1d1337084fe9f445e779946eeba52ffd413f4d7c
mrfinker <caalebs@gmail.com>
2024-01-31 11:55:19 +0000

    31 janv. 2024, 12:55

---
+++ index.js
@@ -0,0 +1,8 @@
+// BASICS
+alert("Bonjour");
+alert("Caleb Kiangebeni");
+console.log("On est, Mercredi 31 Janvier 2024")
+document.body.innerHTML=('Bonjour je suis l\'element attendus sur le body')
+console.log(9+2)
+alert(2+2)
+console.log("Je suis un nombre");
