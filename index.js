7d242d085f702001e4ca5e2ff140fbd548e69bc8
mrfinker <caalebs@gmail.com>
2024-01-31 22:29:10 +0000

    31 janv. 2024, 23:29

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ let qt = 0;
 let myName = "Caleb Kiangebeni";
 console.log(`My name is : ${myName}`);
 let cost = ((1*5)+(2*3)+(1*9));
-let costPercentage = ((1*5)+(2*3)+(1*9) * 0.1);
+let costPercentage = (((1*5)+(2*3)+(1*9)) * 0.1);
 console.log(`Cost of food : $${cost}`);
 console.log(`Tax 10% : $${costPercentage}`);
