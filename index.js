ce7a23305263d1ce1662dbacaeaee792ebc47715
mrfinker <caalebs@gmail.com>
2024-01-31 12:13:01 +0000

    31 janv. 2024, 13:13

--- index.js
+++ index.js
@@ -8,7 +8,19 @@
 // console.log("Je suis un nombre");
 
 // 2.Numbers and Math
-let cel = 9
-let far = 10
-alert("La valeur de "+ cel +` Celsius en Faraneiht est : ${(far - 32) * 5 / 9} `)
-alert("La valeur de "+ far +` Faraneiht en Celsius est : ${(cel * 9 / 5) + 32}`)
+// let cel = 9
+// let far = 10
+// alert("La valeur de "+ cel +` Celsius en Faraneiht est : ${(far - 32) * 5 / 9} `)
+// alert("La valeur de "+ far +` Faraneiht en Celsius est : ${(cel * 9 / 5) + 32}`)
+
+// 3.Strings
+console.log("Caleb" + " Kiangebeni")
+console.log("Caleb" + " Kiangebeni" + " Demissa")
+console.log(typeof(2))
+console.log(typeof(2.2))
+console.log(typeof(2,2))
+console.log(typeof("Bonjour"))
+console.log("hello"+3)
+console.log("$" + 500 + 300)
+console.log("$500" + 300)
+console.log("$" + (500 + 300))
