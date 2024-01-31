5d2b31f874b10119b2da1eb6b6c17bb4359d9274
mrfinker <caalebs@gmail.com>
2024-01-31 12:07:59 +0000

    31 janv. 2024, 13:07

--- index.js
+++ index.js
@@ -10,5 +10,5 @@
 // 2.Numbers and Math
 let cel = 9
 let far = 10
-console.log("La valeur de "+ cel +" Celsius en Faraneiht est : "(far - 32) * 5 / 9)
-console.log("La valeur de "+ far +" Faraneiht en Celsius est : "(celsius * 9 / 5) + 32)
+alert("La valeur de "+ cel +` Celsius en Faraneiht est : ${(far - 32) * 5 / 9} `)
+alert("La valeur de "+ far +` Faraneiht en Celsius est : ${(cel * 9 / 5) + 32}`)
