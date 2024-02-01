a0648e0275f66d1979d357344338ea75d9f8e7b6
mrfinker <caalebs@gmail.com>
2024-02-01 12:55:38 +0000

    1 févr. 2024, 13:55

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
 // 8. Objects
-const product = {
-  name: "Caleb",
-  price: 150
-}
+// const product = {
+//   name: "Caleb",
+//   price: 150
+// }
 
-console.log(typeof product);
-console.log(product);
-console.log(product.name);
-console.log(product.price);
+// console.log(typeof product);
+// console.log(product);
+// console.log(product.name);
+// console.log(product.price);
 
-product.name = 'Change name';
-console.log(product);
+// product.name = 'Change name';
+// console.log(product);
 
-product.newPropriete = true;
-console.log(product);
+// product.newPropriete = true;
+// console.log(product);
 
-delete product.newPropriete;
-console.log(product);
+// delete product.newPropriete;
+// console.log(product);
 
-const product2 = {
-  name: 'Shirt',
-  price: 15,
-  ['delivery']: '1 day',
-  rate: {
-    stars: 4.5,
-    count: 185
-  },
-  fun: function function1(){
-    console.log('C\'est une fonction dans un object');
-  }
-};
+// const product2 = {
+//   name: 'Shirt',
+//   price: 15,
+//   ['delivery']: '1 day',
+//   rate: {
+//     stars: 4.5,
+//     count: 185
+//   },
+//   fun: function function1(){
+//     console.log('C\'est une fonction dans un object');
+//   }
+// };
 
-console.log(product2);
-console.log(product2['name']);
-console.log(product2['delivery']);
-// same things
-console.log(product2.rate.count);
-console.log(product2['rate'].count);
-console.log(product2['rate']['count']);
-console.log(product2.rate['count']);
+// console.log(product2);
+// console.log(product2['name']);
+// console.log(product2['delivery']);
+// // same things
+// console.log(product2.rate.count);
+// console.log(product2['rate'].count);
+// console.log(product2['rate']['count']);
+// console.log(product2.rate['count']);
 
-console.log(product2.fun());
+// console.log(product2.fun());
 
-const reusableJSONProduct2 = JSON.stringify(product2);
-console.log(reusableJSONProduct2);
+// const reusableJSONProduct2 = JSON.stringify(product2);
+// console.log(reusableJSONProduct2);
 
-const reusableObjectProduct2 = JSON.parse(reusableJSONProduct2);
-console.log(reusableObjectProduct2);
+// const reusableObjectProduct2 = JSON.parse(reusableJSONProduct2);
+// console.log(reusableObjectProduct2);
 
 // console.log(product2.delivery-time);
+
