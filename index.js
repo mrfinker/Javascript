a4c470908cb05ab1e4483268034aa297297e6fcc
mrfinker <caalebs@gmail.com>
2024-02-01 11:42:29 +0000

    1 févr. 2024, 12:42

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const product = {
   price: 150
 }
 
+console.log(typeof product);
 console.log(product);
 console.log(product.name);
 console.log(product.price);
 
 product.name = 'Change name';
-console.log(product.name);
+console.log(product);
+
+product.newPropriete = true;
+console.log(product);
+
+delete product.newPropriete;
+console.log(product);
