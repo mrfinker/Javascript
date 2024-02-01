7dbd4e8f1795739e74e14cbd316af71c81789789
mrfinker <caalebs@gmail.com>
2024-02-01 09:58:28 +0000

    1 févr. 2024, 10:58

--- index.js
+++ index.js
@@ -104,66 +104,66 @@ Multi-ligne */
 // message = undefined || 'USD'
 // console.log(message);
 
-win = 0;
-losse = 0;
-tie = 0;
+// win = 0;
+// losse = 0;
+// tie = 0;
 
-let computerNumber = '';
-let result = '';
+// let computerNumber = '';
+// let result = '';
 
-function playerGame(playerMove){
+// function playerGame(playerMove){
   
-  if(playerMove === 'Pierre'){
-    if(computerMove === 'Pierre'){
-      result = ('Personne n\'a gagner');
-      tie += 1;
-    } else if (computerMove === 'Feuille'){
-      result = ('L\'IA a gagner');
-      losse += 1;
-    } else if(computerMove === 'Ciseau') {
-      result = ('Tu as gagner');
-      win += 1;
-    }
+//   if(playerMove === 'Pierre'){
+//     if(computerMove === 'Pierre'){
+//       result = ('Personne n\'a gagner');
+//       tie += 1;
+//     } else if (computerMove === 'Feuille'){
+//       result = ('L\'IA a gagner');
+//       losse += 1;
+//     } else if(computerMove === 'Ciseau') {
+//       result = ('Tu as gagner');
+//       win += 1;
+//     }
 
-  } else if(playerMove === 'Feuille'){
-    if(computerMove === 'Pierre'){
-      result = ('Tu as gagner');
-      win += 1;
-    } else if (computerMove === 'Feuille'){
-      result = ('Match nul');
-      tie += 1;
-    } else if(computerMove === 'Ciseau') {
-      result = ('Tu as perdus');
-      losse += 1;
-    } 
-  } else if(playerMove === 'Ciseau'){
-    if(computerMove === 'Pierre'){
-      result = ('Tu as perdus');
-      losse += 1;
-    } else if (computerMove === 'Feuille'){
-      result = ('Tu as gagner');
-      win += 1;
-    } else if(computerMove === 'Ciseau') {
-      result = ('Match nul');
-      tie += 1;
-    }
-  }
-  alert(`vous avez choisis ${playerMove},  L\'IA a choisis ${computerMove}, ${result}`);
-}
+//   } else if(playerMove === 'Feuille'){
+//     if(computerMove === 'Pierre'){
+//       result = ('Tu as gagner');
+//       win += 1;
+//     } else if (computerMove === 'Feuille'){
+//       result = ('Match nul');
+//       tie += 1;
+//     } else if(computerMove === 'Ciseau') {
+//       result = ('Tu as perdus');
+//       losse += 1;
+//     } 
+//   } else if(playerMove === 'Ciseau'){
+//     if(computerMove === 'Pierre'){
+//       result = ('Tu as perdus');
+//       losse += 1;
+//     } else if (computerMove === 'Feuille'){
+//       result = ('Tu as gagner');
+//       win += 1;
+//     } else if(computerMove === 'Ciseau') {
+//       result = ('Match nul');
+//       tie += 1;
+//     }
+//   }
+//   alert(`vous avez choisis ${playerMove},  L\'IA a choisis ${computerMove}, ${result}`);
+// }
 
-function pickandroidMove(){
-  const randomNumber = Math.random();
+// function pickandroidMove(){
+//   const randomNumber = Math.random();
 
-  if (randomNumber >= 0 && randomNumber < 1/3){
-    computerMove = ('Pierre');
-  } else if(randomNumber >= 1/3 && randomNumber < 2/3){
-    computerMove = ('Feuille');
-  } else if(randomNumber >= 2/3 && randomNumber < 1){
-    computerMove = ('Ciseau');
-  }
+//   if (randomNumber >= 0 && randomNumber < 1/3){
+//     computerMove = ('Pierre');
+//   } else if(randomNumber >= 1/3 && randomNumber < 2/3){
+//     computerMove = ('Feuille');
+//   } else if(randomNumber >= 2/3 && randomNumber < 1){
+//     computerMove = ('Ciseau');
+//   }
 
-  return console.log(computerMove);
-}
+//   return console.log(computerMove);
+// }
 
 // let hour = 18;
 // let name = 'Caleb Kiangebeni'
