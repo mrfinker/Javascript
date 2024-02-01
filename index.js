a6af0209c7d61429f26e532c80a28f73a97a4bef
mrfinker <caalebs@gmail.com>
2024-02-01 16:35:51 +0000

    1 févr. 2024, 17:35

--- index.js
+++ index.js
@@ -10,4 +10,6 @@
 // console.log(document.body);
 // console.log(typeof document.body);
 
-console.log(document.body.innerHTML);
+// console.log(document.body.innerHTML);
+// document.body.innerHTML=('changed')
+
