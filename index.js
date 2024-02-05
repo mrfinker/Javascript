7236573d722d7e03131814ff2738d2591a360dad
mrfinker <caalebs@gmail.com>
2024-02-05 07:30:56 +0000

    5 févr. 2024, 08:30

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // 11. Arrays and Loops
+const array = [10, 25, 45, 55, 60];
+console.log(array);
+console.log(array[0]);
 [10, 25, 45, 55, 60].forEach((value, index) => console.log(`Value : ${value} , Index : ${index}`))
