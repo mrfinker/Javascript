fb114d000088b854a77489d443b484f7fc26ae97
mrfinker <caalebs@gmail.com>
2024-02-05 07:58:48 +0000

    5 févr. 2024, 08:58

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ console.log(myArray[0]);
 console.log(myArray[2]);
 myArray[0] = 86;
 console.log(myArray);
-[10, 25, 45, 55, 60].forEach((value, index) => console.log(`Value : ${value} , Index : ${index}`))
+[10, 25, 45, 55, 60].forEach((value, index) =>
+  console.log(`Value : ${value} , Index : ${index}`)
+);
