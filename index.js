2d3f40987ba1cd15e59f57a0ddeb43af2a2e590b
mrfinker <caalebs@gmail.com>
2024-02-05 06:53:30 +0000

    5 févr. 2024, 07:53

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function isToggle(){
   let defaultValue = document.querySelector('.change');
-  console.log(defaultValue);
+  console.log(defaultValue.innerHTML);
+  if(defaultValue.innerHTML === 'ON'){
+    defaultValue.innerHTML = 'OFF';
+  } else if(defaultValue.innerHTML === 'OFF'){
+    defaultValue.innerHTML = 'ON';
+  }
 }
