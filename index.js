6784062c793717960a0b6e66ca0576698d26f967
mrfinker <caalebs@gmail.com>
2024-02-12 12:18:21 +0000

    12 févr. 2024, 13:18

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-12
+// 12. advanced functions
+
+function greeting(){
+  console.log('hello');
+}
+
+greeting();
+
+const num = 2;
+
+const function1 = function greeting(){
+  console.log('hello2');
+};
+
+console.log(function1);
