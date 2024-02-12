2e356f44b09c27dbcd3eb28b66e87f1a209cea87
mrfinker <caalebs@gmail.com>
2024-02-12 12:27:14 +0000

    12 févr. 2024, 13:27

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ greeting();
 
 const num = 2;
 
+// version 1
 // const function1 = function greeting(){
 //   console.log('hello2');
 // };
 
+// version 2
 const function1 = function() {
   console.log('hello2');
 };
@@ -20,3 +22,32 @@ console.log(function1);
 console.log(typeof function1);
 
 function1();
+
+const obj1 = {
+  num: 2,
+  // version1
+  // fun: function greeting(){
+  //   console.log('hello3');
+  // }
+  // version2
+  fun: function(){
+    console.log('hello3');
+  }
+}
+
+obj1.fun();
+
+function display(param){
+  console.log(param);
+}
+display('yeah');
+
+
+// callback function
+function run(param){
+  param();
+}
+
+run(function(){
+  console.log('Appelle function dans une function en tant que parametre');
+})
