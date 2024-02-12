380822e7c45983157d5e02a8c88dd708d3861d65
mrfinker <caalebs@gmail.com>
2024-02-12 14:24:45 +0000

    12 févr. 2024, 15:24

--- index.js
+++ index.js
@@ -68,9 +68,14 @@ setInterval(function(){
   console.log('interval1');
 }, 3000);
 */
-let level;
+
 let valueDoc = document.querySelector(".userValue");
-let randomValue = Math.round(Math.random() * Number(level));
+
+
+function levelBtn(){
+  let level;
+  let randomValue = Math.round(Math.random() * Number(level));
+}
 
 function checkValue() {
   let valueUser;
