6947f50231154d641f7a6608160dd3397226c8d5
mrfinker <caalebs@gmail.com>
2024-02-15 18:58:39 +0000

    15 févr. 2024, 19:58

---
+++ index.js
@@ -0,0 +1,25 @@
+// const myfunc = () => {
+//   console.log('arrow function');
+// }
+/*
+const myfunc = (param) => {
+  console.log('arrow function');
+  console.log(param + 1);
+}
+myfunc(2)
+
+const regularfunc = function(){
+  console.log('regular function');
+}
+regularfunc()
+
+const obj1 = {
+  // sont identiques
+  method: () => {
+
+  },
+  method(){
+
+  }
+}
+*/
